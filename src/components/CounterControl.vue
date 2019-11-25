0e3ffd8176a49b08103869c88c1c66ffe49fc22d
Dong-Eun-Lee <50562381+Dong-Eun-Lee@users.noreply.github.com>
2019-11-25 18:01:58 +0900

    Prop Counter

---
+++ src/components/CounterControl.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button @click="increament">
+      +
+    </button>
+    <button @click="decreament">
+      -
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class CounterControl extends Vue {
+  @Prop() public cnt: number = 0;
+  public count: number;
+  constructor() {
+    super();
+
+    this.count = this.cnt;
+  }
+
+  public increament() {
+    this.count++;
+    this.$emit("emitEvent", this.count);
+  }
+
+  public decreament() {
+    this.count--;
+    this.$emit("emitEvent", this.count);
+  }
+}
+</script>
---
+++ src/components/CounterView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ cnt }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class CounterView extends Vue {
+  @Prop() public cnt: number;
+  constructor() {
+    super();
+
+    this.cnt = 0;
+  }
+}
+</script>
